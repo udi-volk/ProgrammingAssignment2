7ecb9c1f5bea8b3c392e4fc5c0ed4fd24f70e266
udi-volk <udi.volk@gmail.com>
2014-10-26 11:56:24 +0200

    Functions to cache matrix inverse
    File contains assignment 2 that teaches us about the <<- operator and
    about environments. The file contains two functions one to create the
    matrix and its interface (thru a list) and the other to retrieve its
    inverse or calculate it if needed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The functions allow one to create a matrix that stores its
+## inverse for future use without having to calculate the inverse
+## every time it is required but rather retrieve it from memory if it has
+## been calculated before. The set must use the <<- assignement operator to access the matrix 
+## in its creation environment - using <- will not work :)
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Creates a cached Matrix with Inversed cache capability
+  ## returns a list of all the available function on the matrix
+  
+  i <- NULL
+  set <- function(y) {
+    ## Looking for x in enclosing environment for deep assignment
+    x <<- y
+    ## Same deep assignment; Matrix changed and therefore inverse is nulled
+    i <<-  NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  ## The value returned by the function is its list of "public" functions
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## The function return the inverse of x as well as store the
+  ## the result for future use
+  ## x is a cached Matrix, ... additional parameters 
+  ## for solve(inverse function)
+  ## See help(solve) for possible values 
+  i=x$getInverse()
+  if(!is.null(i)){
+    ## Inverse has been stored before
+    message("getting cached data")
+    return(i)
+  }
+  ## Inverses has not been cached before therefore need to get it
+  data=x$get()
+  ## Solve the Inverse pass along the parameters ...
+  i=solve(data,...)
+  ## Need to store the Inverse into the cache
+  x$setInverse(i)
+  i
 }
